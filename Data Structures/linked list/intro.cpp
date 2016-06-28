2d8637a11dbf72a12e651fd29c0f33631c281a0b
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-28 13:23:10 +0530

    Linked List insertion

---
+++ Data Structures/linked list/intro.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+	int data;
+	struct node* next;
+};
+
+void inbeginning( node** head_ref,int data)
+{
+	node *new_node = new node;
+	new_node->data = data;
+	new_node->next = *head_ref;
+	*head_ref = new_node;
+}
+
+void after_some(node** head_ref,int pos,int data)
+{
+	node *new_node = new node;
+	new_node->data = data;
+	node *toPt = *head_ref;
+	//node *toPt1 = *head_ref;
+	for(int i=0;i<pos-1;i++)
+	{
+		toPt = toPt->next;
+	}
+	new_node->next = toPt->next;
+	toPt->next = new_node;
+	
+}
+
+void inEnd(node** head_ref,int data)
+{
+//	cout<<"yo "<<*head_ref<<" "<<head_ref<<"yo";
+	node *new_node = new node;
+	new_node->data = data;
+	new_node->next = NULL;
+	node *last = *head_ref;
+	
+	if(*head_ref == NULL)
+	{
+		*head_ref = new_node;
+		return ;
+	}
+	while(last->next!=NULL)
+	{
+		//cout<<last->data<<" ";
+		last= last->next;
+	}
+	last->next = new_node;
+	return;
+}
+
+void printList( node *node)
+{
+  while (node != NULL)
+  {
+     printf(" %d ", node->data);
+     node = node->next;
+  }
+}
+int main()
+{
+	node *head = NULL;
+	inbeginning(&head,5);
+	inbeginning(&head,4);
+	inEnd(&head,3);
+	inbeginning(&head,2);
+	inbeginning(&head,1);
+	after_some(&head,3,8);
+	printList(head);
+	
+}
